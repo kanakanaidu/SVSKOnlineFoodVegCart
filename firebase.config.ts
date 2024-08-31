9a44babb970fb186e018b2b2618cf27146ca0446
Kanakaraju Thota <kanaka_share@hotmail.com>
2024-08-31 06:52:09 +0530

    different environment variables used for build

--- firebase.config.ts
+++ firebase.config.ts
@@ -6,22 +6,12 @@ import { getStorage } from "firebase/storage";
 // const apps = getApps();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAmZ8Atxnffoqa-xPcVpdg1VIoQ3Bx1mSo",
-  authDomain: "lepakshionlinestore-abcd.firebaseapp.com",
-  projectId: "lepakshionlinestore-abcd",
-  storageBucket: "lepakshionlinestore-abcd.appspot.com",
-  messagingSenderId: "821996480852",
-  appId: "1:821996480852:web:baedd0a4b7a0cc4e5bc0c1",
-  measurementId: "G-N0P3SNW8X7"
-
-  // apiKey: "AIzaSyAii_VVuGuJjWJKZRmwV3QkCV5CWjYUpNg",
-  // authDomain: "svskonline-ba87d.firebaseapp.com",
-  // databaseURL: "https://svskonline-ba87d-default-rtdb.firebaseio.com",
-  // projectId: "svskonline-ba87d",
-  // storageBucket: "svskonline-ba87d.appspot.com",
-  // messagingSenderId: "118736530068",
-  // appId: "1:118736530068:web:cb94cbd6306f2776d0e281",
-  // measurementId: "G-641DBRG3WR",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
